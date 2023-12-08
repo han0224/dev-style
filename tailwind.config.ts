9b99bed3d10bc5dce2497fb55692e32fce232569
han0224 <dbn0224@gmail.com>
2023-12-08 17:24:34 +0900

    tailwind color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,25 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    textColor: {
+      white: "#FFF",
+      black: "#000",
+    },
+    colors: {
+      primary: {
+        "50": "#f0fdfa",
+        "100": "#ccfbf1",
+        "200": "#99f6e4",
+        "300": "#5eead4",
+        "400": "#2dd4bf",
+        "500": "#14b8a6",
+        "600": "#0d9488",
+        "700": "#0f766e",
+        "800": "#115e59",
+        "900": "#134e4a",
+        "950": "#042f2e",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
