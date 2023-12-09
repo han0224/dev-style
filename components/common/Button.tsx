9b434212d156459e5cf73497999db60d70b5e7b3
han0224 <dbn0224@gmail.com>
2023-12-10 01:30:33 +0900

    button component

---
+++ components/common/Button.tsx
@@ -0,0 +1,16 @@
+type TButton = {
+  event?: React.MouseEventHandler<HTMLButtonElement>;
+  text: string;
+};
+
+export default function Button(props: TButton) {
+  return (
+    <button
+      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+                            dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+      onClick={props.event}
+    >
+      {props.text}
+    </button>
+  );
+}
