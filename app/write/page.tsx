df727ce02e31928e3d57a8ab502f255e52510ba1
han0224 <dbn0224@gmail.com>
2023-12-17 16:26:38 +0900

    useRef -> useState

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,53 +1,53 @@
 "use client";
 
-import { useRef } from "react";
-import { Textarea } from "@/components/write";
+import { useEffect, useState } from "react";
+import { Editor } from "@/components/write";
 import { Button } from "@/components/common";
 
 export default function Write() {
-  const previewRef = useRef<HTMLIFrameElement>(null);
-  const htmlRef = useRef<HTMLTextAreaElement>(null);
-  const cssRef = useRef<HTMLTextAreaElement>(null);
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
 
-  const getPreview = () => {
-    if (!previewRef.current || !previewRef.current.contentDocument) return;
+  const [srcDoc, setSrcDoc] = useState("");
 
-    const html = htmlRef.current?.value;
-    const css = cssRef.current?.value;
-
-    previewRef.current.contentDocument.body.innerHTML =
-      html + "<style>" + css + "</style>";
-  };
+  useEffect(() => {
+    setSrcDoc(
+      `
+      <html>
+        <style>${css}</style>
+        <body>${html}</body>
+      </html>
+      `
+    );
+  }, [html, css]);
 
   return (
-    <div className="flex flex-col h-full w-full mt-16">
-      <div className="flex gap-2">
+    <div className="flex-1 flex flex-col h-full w-full mt-16">
+      <div className="flex-1 flex gap-2">
         <div className="flex-1 flex flex-col justify-between gap-2">
-          <div className="flex-1">
-            <Textarea
-              keyupEvent={getPreview}
-              id="html"
-              name="html"
+          <div className="h-1/2">
+            <Editor
+              onChange={setHtml}
+              value={html}
               label="html"
-              ref={htmlRef}
               icon="html"
+              language="html"
             />
           </div>
-          <div className="flex-1">
-            <Textarea
-              keyupEvent={getPreview}
-              id="css"
-              name="css"
+          <div className="h-1/2">
+            <Editor
+              onChange={setCss}
+              value={css}
               label="css"
-              ref={cssRef}
               icon="css"
+              language="css"
             />
           </div>
         </div>
         <div className="flex-1">
           <iframe
+            srcDoc={srcDoc}
             className="p-2.5 w-full h-full text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  resize-none"
-            ref={previewRef}
           ></iframe>
         </div>
       </div>
